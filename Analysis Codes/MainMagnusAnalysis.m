ebbf959ec582d0e1a549621b32589d7b5b2952ba
Feng <zf1005@wildcats.unh.edu>
2017-05-01 13:42:10 -0400

    created folders and added related files:

---
+++ Analysis Codes/MainMagnusAnalysis.m
@@ -0,0 +1,14 @@
+clear all;
+close all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Authors: Feng, Zhangxi; Popecki, Simon; Skinner, James %
+% Course: ME646                                          %
+% Project: Cylindrical Airfoil Magnus Effect             %
+% Date: May 1, 2017                                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Magnus Effect Overview
+% Variables:
+% FL: Lift Force
+% 
